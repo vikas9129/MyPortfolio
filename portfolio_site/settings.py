0df9c1b732be5e33111257d34a8609717db20cd3
vikas9129 <vniteesh9129@gmail.com>
2026-09-20 18:59:55 +0530

    Restore changes

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -84,8 +84,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR]
-STATIC_ROOT = BASE_DIR.parent / "portfolio_staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
--- staticfiles/staticfiles/portfolio_site/settings.py
+++ staticfiles/staticfiles/portfolio_site/settings.py
@@ -86,6 +86,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_IGNORE_PATTERNS = ["CVS", ".*", "*~", "staticfiles", "staticfiles/**"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
