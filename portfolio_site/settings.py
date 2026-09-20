6c3ad729f464599babdf963d46f7d6ab95d4a06e
vikas9129 <vniteesh9129@gmail.com>
2026-09-20 18:47:24 +0530

    Pata nhi kya change kiya

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR.parent / "portfolio_staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
